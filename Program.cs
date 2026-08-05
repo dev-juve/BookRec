f788ccac13f7b6dda7b75af1bc20417a616db2bb
ldsjayhawk <33238407+ldsjayhawk@users.noreply.github.com>
2026-08-04 20:54:31 -0500

    Added authentication

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using BookRec.Components;
+using AspNet.Security.OAuth.GitHub;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,26 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddSingleton<BookRec.Services.BookService>();
 
+// Add authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = "Cookies";
+    options.DefaultSignInScheme = "Cookies";
+    options.DefaultChallengeScheme = "GitHub";
+})
+.AddCookie("Cookies")
+.AddGitHub(options =>
+ {
+    options.ClientId = builder.Configuration["Authentication:GitHub:ClientId"]!;
+    options.ClientSecret = builder.Configuration["Authentication:GitHub:ClientSecret"]!;
+    options.Scope.Add("user:email");
+ });
+
+builder.Services.AddAuthorization();
+builder.Services.AddCascadingAuthenticationState();
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -19,6 +40,26 @@ if (!app.Environment.IsDevelopment())
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
+
+// for authentication
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/signin", async (HttpContext context) =>
+{
+    await context.ChallengeAsync("GitHub",
+    new AuthenticationProperties
+    {
+        RedirectUri = "/"
+    });
+});
+
+app.MapGet("/signout", async (HttpContext context) => 
+{
+    await context.SignOutAsync("Cookies");
+    context.Response.Redirect("/");
+});
 
 app.UseAntiforgery();
 
